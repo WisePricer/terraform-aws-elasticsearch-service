298a9521ab121bd22f851f275bb40d0434b6acdd
Steven Nemetz <snemetz@wiser.com>
2017-12-08 14:28:59 -0800

    Fix polixy

--- main.tf
+++ main.tf
@@ -4,9 +4,6 @@
 # https://www.terraform.io/docs/providers/aws/r/elasticsearch_domain.html
 # https://www.terraform.io/docs/providers/aws/r/elasticsearch_domain_policy.html
 
-data "aws_iam_policy_document" "es_management_access" {
-
-}
 resource "aws_elasticsearch_domain" "this" {
   domain_name = "${var.namespaced ? format("%s-%s", var.environment, var.name) : format("%s", var.name)}"
   elasticsearch_version = "${var.version}"
@@ -37,6 +34,20 @@ resource "aws_elasticsearch_domain" "this" {
     map("Terraform", "true") )}"
 }
 
+/*
+# Define or retrieve IAM Policy?
+resource "aws_iam_policy" "" {
+  name        = ""
+  path        = ""
+  description = ""
+  policy  = ""
+}
+# return: id, arn, description, name, path, policy
+#access_policies = "${aws_iam_policy..policy}"
+
+data "aws_iam_policy_document" "es_management_access" {
+}
+*/
 resource "aws_elasticsearch_domain_policy" "this" {
   domain_name = "${aws_elasticsearch_domain.this.domain_name}"
   #access_policies = "${data.aws_iam_policy_document.es_management_access.json}"
@@ -52,7 +63,7 @@ resource "aws_elasticsearch_domain_policy" "this" {
             "Condition": {
                 "IpAddress": {"aws:SourceIp": "127.0.0.1/32"}
             },
-            "Resource": "${aws_elasticsearch_domain.example.arn}"
+            "Resource": "${aws_elasticsearch_domain.this.arn}"
         }
     ]
 }
